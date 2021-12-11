f02ddbf2971600009c59acc6a160158cbc47951e
Michał Tomasik <mtomasik@inspirion-popos.home>
2021-12-11 09:42:56 +0100

    Empty project

---
+++ src/test/java/com/tomasikm94/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.tomasikm94;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+	
+	// Execute the same tests but in native mode.
+}
